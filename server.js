699b7e7949d3736fdccf4b0ae2987a1af4840636
carlosnight02 <ca.murillo1@hotmail.com>
2020-01-10 16:53:38 -0500

    hbs parciales

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 
+const hbs = require('hbs');
+
 app.use(express.static(__dirname+'/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname+ '/views/parciales');
+app.set('view engine', 'hbs');
  
-/*app.get('/', (req, res)=> {
-  let salida = {
-      nombre: 'fernando',
-      edad: 32,
-      url: req.url
-  };
-  res.send(salida);
-})*/
+app.get('/', (req, res)=> {
+  
+  res.render('home', {
+    nombre: 'Carlos',
+    anio: new Date().getFullYear()  
+  });
+});
+
+app.get('/about', (req, res) => {
+
+  res.render('about', {
+    anio: new Date().getFullYear()
+   });
+
+});
+
+
+
 const port= 3000; 
 app.listen(port, ()=>{
     console.log(`Escuchando en el puerto ${port}`);

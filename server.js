0adce37397ab7ef8a68376e77243e91a2e9f7a88
carlosnight02 <ca.murillo1@hotmail.com>
2020-01-15 17:08:23 -0500

    Helpers001

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const hbs = require('hbs');
+const hbs = require("hbs");
+require('./hbs/helpers');
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + "/public"));
 
 //Express HBS engine
-hbs.registerPartials(__dirname+ '/views/parciales');
-app.set('view engine', 'hbs');
- 
-app.get('/', (req, res)=> {
-  
-  res.render('home', {
-    nombre: 'Carlos',
-    anio: new Date().getFullYear()  
+hbs.registerPartials(__dirname + "/views/parciales");
+app.set("view engine", "hbs");
+
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "carLos aNdreS muRilLo"
   });
 });
 
-app.get('/about', (req, res) => {
-
-  res.render('about', {
-    anio: new Date().getFullYear()
-   });
-
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-
-
-const port= 3000; 
-app.listen(port, ()=>{
-    console.log(`Escuchando en el puerto ${port}`);
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
+});
